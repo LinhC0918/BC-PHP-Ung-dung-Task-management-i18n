b611bfa716d97fa55267e5bd29239ef80c567f67
LinhC0918 <nguyenvanlinh12d3@gmail.com>
2018-11-30 14:00:07 +0700

    Add new file

---
+++ app/Http/Middleware/Locale.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Session;
+use Closure;
+
+class Locale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Session::has('language')) {
+            Session::put('language', config('app.locale'));
+        }
+        Lang::setLocale(Session::get('language'));
+        return $next($request);
+    }
+}
---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,63 @@
+@extends('home')
+
+@section('title', 'Cập nhật công viêc')
+
+@section('content')
+
+    <div class="row">
+
+        <div class="col-md-12">
+
+            <h2>{!! __('language.task') !!}</h2>
+
+        </div>
+
+        <div class="col-md-12">
+
+            <form method="post" action="{{ route('tasks.update', $task->id) }}" enctype="multipart/form-data">
+
+                @csrf
+
+                <div class="form-group">
+
+                    <label>{!! __('language.name') !!}</label>
+
+                    <input type="text" class="form-control" name="title" value="{{ $task->title }}" required>
+
+                </div>
+
+                <div class="form-group">
+
+                    <label>{!! __('language.content') !!}</label>
+
+                    <textarea class="form-control" rows="3" name="content"  required>{{ $task->content }}</textarea>
+
+                </div>
+
+                <div class="form-group">
+
+                    <label>{!! __('language.image') !!}</label>
+
+                    <input type="file" name="image" class="form-control-file" >
+
+                </div>
+
+                <div class="form-group">
+
+                    <label>{!! __('language.due_date') !!}</label>
+
+                    <input type="date" name="due_date" class="form-control"  value="{{ $task->due_date }}" required>
+
+                </div>
+
+                <button type="submit" class="btn btn-primary">{!! __('language.edit') !!}</button>
+
+                <button class="btn btn-secondary" onclick="window.history.go(-1); return false;">{!! __('language.cancel') !!}</button>
+
+            </form>
+
+        </div>
+
+    </div>
+
+@endsection
